b1ccc1e7e23297f00c331bda215053de4eef283f
Kyle Kurz <kyle@doublekaudio.com>
2014-10-22 18:32:17 -0400

    Display string update

--- src/js/server.js
+++ src/js/server.js
@@ -2,6 +2,6 @@ var Connection = require('./connection.js');
 $(document).ready(function () {
 	var connection = new Connection({id:'server', target:'client'});
 	connection.init().then(function () {
-		$('body').html('<div class="subBlock center role-small"><span class="centered-small">Connected</span></div>');
+		$('body').html('<div class="subBlock center role-small"><span class="centered-small">Connected to Respoke</span></div>');
 	});
 });
